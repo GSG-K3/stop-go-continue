7d2414c2cbd25ef5718249965f7ea9e69f0d9c31
nidaa-awawdeh <nidaaawawdeh@outlook.sa>
2020-03-03 15:31:43 +0200

    databasebuild

---
+++ src/models/database/db_connection.js
@@ -0,0 +1,14 @@
+const {Pool} = require('pg');
+
+ require('env2')('.env')
+  const connectionString =process.env.DATABASE_URL
+
+if (!connectionString){
+    console.log(' unsuccessful connection')
+}
+
+
+module.exports = new Pool({
+   connectionString,
+   ssl: !connectionString.includes("localhost") 
+});
