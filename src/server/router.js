f7c17566144a877ba78189ce3d43c74704bd926a
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-03-03 15:52:43 +0200

    home section,login form,register form >#5,#6,#8

---
+++ src/server/router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const db = require('../models/database/db_build');
+const bodyParser = require('body-parser');
+const postData = require('../models/database/queries/postData');
+const getData = require('../models/database/queries/getData');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.post('/register', (req, response) => {
+    // postData((err, res) => {
+    //     if (err) { console.log(err, "cannot connecting") }
+    //     else {
+    //         console.log(req.body);
+    //         response.redirect('/')
+    //     }
+    // });
+    console.log(req.body);
+    response.redirect('/');
+});
+router.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'..','..','public','home.html'))
+})
+
+
+module.exports = router;
