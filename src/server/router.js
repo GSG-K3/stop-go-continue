b01cb66d181d93b6a5ca390c232debe83fc84b0f
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-03-04 13:38:59 +0200

    fix forms

--- src/server/router.js
+++ src/server/router.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const db = require("../models/database/db_build");
 const bodyParser = require("body-parser");
 const postData = require("../models/database/queries/postData");
 const postActivity = require("../models/database/queries/postactivity");
@@ -10,20 +9,18 @@ const getData = require("../models/database/queries/getData");
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post('/register', (req, response) => {
-    console.log(req.body);
-    postData(req.body);
-    response.redirect('/');
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "public", "home.html"));
+});
+
+router.post("/register", (req, response) => {
+  postData(req.body);
+  response.redirect("/login.html");
 });
 
 router.post("/student", (req, response) => {
-  console.log(req.body);
-  postActivity( req.body);
+  postActivity(req.body);
   response.redirect("/student.html");
-});
-
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "public", "home.html"));
 });
 
 module.exports = router;
