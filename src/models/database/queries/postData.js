9e169e3061fb39f34562cfbd6a07cd7b7e702115
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-03-04 09:42:32 +0200

    insert into user table #19

---
+++ src/models/database/queries/postData.js
@@ -0,0 +1,12 @@
+const connection = require('../db_connection');
+const postData = (reqBody) => {
+    const { name, email, password, type} = reqBody;
+    sql = {
+        text: "INSERT INTO user_info(name,email,password,type) VALUES($1,$2,$3,$4);",values: [name, email, password,type]
+    }
+    connection.query(sql.text,sql.values,(err,result)=>{
+        if (err) throw err;
+    });
+
+}
+module.exports=postData;
